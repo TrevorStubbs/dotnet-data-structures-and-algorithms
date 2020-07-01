70fd86f62211e8f09723be1b1ecb6f7683f9ed45
Trevor Stubbs <stubbste@gmail.com>
2020-07-01 13:26:39 -0700

    challenge 2 and 3 complete

--- dotnet-prework-code-challenges/README.md
+++ dotnet-prework-code-challenges/README.md
@@ -18,4 +18,41 @@ You can select any number from the list, however your scoring will be depend on 
 >Sample Output
 >8
 
-3. 
+3. ![Challenge1](images/dotnetpre1.png)
+
+## Problem 2
+1. Leap Year Calculator
+2. Problem Statement
+>Given a year, report if it is a leap year.
+
+>The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+>>on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+
+>For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+>Want to know more about Leap Year? Watch this 4 minute video HERE{:target="_blank"}
+
+>Note: This is not a trick problem. Edge cases do not need to be taken into consideration when creating a solution.
+3. ![Challenge2](images/dotnetpre2.png)
+
+## Problem 3
+1. Perfect Sequence
+2. Problem Statement
+>Given an array, return "Yes" if the sequence is considered >a perfect sequence. Otherwise, return "No"
+>
+>Perfect Sequences
+>A perfect sequence is a sequence such that all of its >elements are non-negative integers and the product of all >of them is equal to their sum. For example: [2,2], [1,3,2] >and [0,0,0,0] are perfect sequences and [4,5,6] and [0,2,>-2] are not perfect sequences. Negative numbers of any kind >are not valid in a perfect sequence
+>
+>Input Format
+>An array of integers (may not be sorted)
+>
+>Sample Input
+>[1,3,2]
+>
+>Sample Output
+>Yes
+
+3. ![Challenge3](images/dotnetpre3.png)
--- dotnet-prework-code-challenges/dotnet-prework-code-challenges/Program.cs
+++ dotnet-prework-code-challenges/dotnet-prework-code-challenges/Program.cs
@@ -39,21 +39,90 @@ namespace dotnet_prework_code_challenges
                 }
             }
 
+            Console.Write("Array Output: ");
             for (int i = 0; i < userCreatedArray.Length; i++)
             {
                 // ouput array to console
                 Console.Write($"{userCreatedArray[i]} ");
             }
-
+            Console.WriteLine();
             Console.Write("Enter a number between 1-10: ");
             int userScore = int.Parse(Console.ReadLine());
 
             return ArrayMaxResult(userCreatedArray, userScore);
         }
 
+        // Challenge 2
+
+        public static bool LeapYearCalc(int inputYear)
+        {
+            if (inputYear % 4 == 0 && inputYear % 100 != 0)            
+                return true;            
+            if (inputYear % 400 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static string problem2Caller()
+        {
+            Console.Write("Enter a year: ");
+            int outputYear = int.Parse(Console.ReadLine());
+
+            if (LeapYearCalc(outputYear))
+                return $"{outputYear} is a leap year";
+            else
+                return $"{outputYear} is not a leap year";
+        }
+
+        // Challenge 3
+        public static string PerfectSequence(int[] inputArray)
+        {
+            int sum = 0;
+            int product = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] < 0)
+                    return "No";
+                else if(i == 0)
+                {
+                    sum = inputArray[i];
+                    product = inputArray[i];
+                }
+                else
+                {
+                    sum += inputArray[i];
+                    product *= inputArray[i];
+                }
+
+            }
+            if (sum == product)
+                return "Yes";
+            else
+                return "No";
+        }
+
+        public static void problem3Caller()
+        {
+            int[] numbers0 = { 2, 2 };
+            int[] numbers1 = { 1, 3, 2 };
+            int[] numbers2 = { 0, 0, 0, 0 };
+            int[] numbers3 = { 4, 5, 6 };
+            int[] numbers4 = { 0, 2, -2 };
+            
+            Console.WriteLine($"Problem 3 output: {PerfectSequence(numbers0)}.");
+            Console.WriteLine($"Problem 3 output: {PerfectSequence(numbers1)}.");
+            Console.WriteLine($"Problem 3 output: {PerfectSequence(numbers2)}.");
+            Console.WriteLine($"Problem 3 output: {PerfectSequence(numbers3)}.");
+            Console.WriteLine($"Problem 3 output: {PerfectSequence(numbers4)}.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine($"Problem 1 output: {problem1Caller()}.");
+            //Console.WriteLine($"Problem 1 output: {problem1Caller()}.");
+            //Console.WriteLine($"Problem 2 output: {problem2Caller()}.");
+            problem3Caller();
+
             Console.ReadLine();
         }
     }
